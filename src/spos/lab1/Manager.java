4925ef45a66186c041fdefcb13d90ef0db8eb86c
Bohdan Kopanichuk <danbog2000@gmail.com>
2019-10-29 16:20:45 +0200

    Init project

---
+++ src/spos/lab1/Manager.java
@@ -0,0 +1,16 @@
+package spos.lab1;
+
+import spos.lab1.demo.*;
+import java.util.Scanner;
+
+public class Manager {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter x");
+        int x = in.nextInt();
+/*
+        DoubleOps.funcF(x);
+        DoubleOps.funcG(x);
+*/
+    }
+}
